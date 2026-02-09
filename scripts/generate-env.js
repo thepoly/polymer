e5ad53b126ea6c4c9790855e2a456d430348e9ca
Ronan Hevenor <spacechickenrobot@gmail.com>
2026-02-09 18:14:30 -0500

    workflow fixes

--- scripts/generate-env.js
+++ scripts/generate-env.js
@@ -1,6 +1,6 @@
-const fs = require('fs');
-const path = require('path');
-const crypto = require('crypto');
+import fs from 'fs';
+import path from 'path';
+import crypto from 'crypto';
 
 const envPath = path.join(process.cwd(), '.env');
 
