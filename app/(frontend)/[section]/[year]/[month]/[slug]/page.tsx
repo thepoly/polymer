2a649b3869b3340a0342de0b4957b89271d63b54
Anthony Santiago <santia6@rpi.edu>
2026-02-25 02:14:23 -0500

    Fix grey text in opinion articles, override to black

--- app/(frontend)/[section]/[year]/[month]/[slug]/page.tsx
+++ app/(frontend)/[section]/[year]/[month]/[slug]/page.tsx
@@ -99,7 +99,7 @@ export default async function ArticlePage({ params }: Args) {
         <OpinionScrollBar title={article.title} />
         <article className="container mx-auto px-4 md:px-6 mt-8 md:mt-12">
           <OpinionArticleHeader article={article} />
-          <div className="max-w-[600px] mx-auto">
+          <div className="max-w-[600px] mx-auto [--foreground-muted:#000000]">
             <ArticleContent content={article.content} />
             <ArticleFooter />
           </div>
