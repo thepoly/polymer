7e5a12e3af38da3997c374fc4f9a5976bd3a462d
Ronan Hevenor <spacechickenrobot@gmail.com>
2026-03-24 21:29:19 -0400

    build fix

--- app/(frontend)/archives/page.tsx
+++ app/(frontend)/archives/page.tsx
@@ -1,1 +1,14 @@
-export { metadata, revalidate, default } from '../archive/page';
+import ArchivePage, { metadata as archiveMetadata, revalidate as archiveRevalidate } from '../archive/page';
+
+export const metadata = {
+  ...archiveMetadata,
+  alternates: { canonical: '/archives' },
+  openGraph: {
+    ...archiveMetadata.openGraph,
+    url: '/archives',
+  },
+};
+
+export const revalidate = archiveRevalidate;
+
+export default ArchivePage;
