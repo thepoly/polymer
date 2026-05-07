d949a005f2c0b6775bf7c202a3cc3e10f7fce096
Ronan Hevenor <spacechickenrobot@gmail.com>
2026-05-07 10:05:26 -0400

    feat(middleware): restore WP-era pluginSL_shorturl redirects (#142)
    The 2013-2014 articles often link to documents via 5-digit shortlinks
    that the WP `pluginSL_shorturl` plugin redirected to external URLs (the
    RPI Student Senate document portal, Google Docs, Dropbox, etc.). Those
    redirects vanished in the polymer cutover, so links inside imported
    article bodies (e.g. "/47288") were 404'ing.
    
    Adds a static map of all 33 shortlinks (extracted from the WP DB dump
    in recon/) and a path matcher in middleware.ts. A few of these targets
    themselves point at old `poly.rpi.edu/YYYY/...` URLs which then chain
    through the existing legacy-permalink redirect to the new polymer URL.
    
    External targets stay as 301s out of poly.rpi.edu.

--- middleware.ts
+++ middleware.ts
@@ -11,7 +11,56 @@ const ARTICLE_URL_RE = /^\/([a-z]+)\/(\d{4})\/(\d{2})\/([a-z0-9][a-z0-9-]*)\/?$/
 // Old WordPress permalink shape from the 2009-2019 era. Years restricted
 // to 2009-2019 so we don't accidentally swallow other paths.
 const LEGACY_WP_URL_RE = /^\/(20(?:0[9]|1[0-9]))\/(\d{2})\/(\d{2})\/([a-z0-9][a-z0-9_-]*)\/?$/
+// 5-digit ID URL shape from the WP-era pluginSL_shorturl plugin. Articles
+// from 2013-2014 link to documents via these short codes.
+const LEGACY_WP_SHORTLINK_RE = /^\/(\d{5})\/?$/
 const VALID_SECTIONS = new Set(['news', 'sports', 'features', 'opinion'])
+
+/**
+ * 33 shortlinks recovered from the legacy WordPress `pluginSL_shorturl` table.
+ * Most of these point at student-senate document portals (still up at
+ * docs.studentsenate.rpi.edu) or external services (Google Docs, Eventbrite,
+ * etc.). Some chain to old `poly.rpi.edu/YYYY/...` URLs that the
+ * LEGACY_WP_URL_RE branch then redirects to their new polymer URL.
+ *
+ * Source: `pluginSL_shorturl` table in
+ * /home/red/poly/recon/archives/wordpress/db/wordpress-archive-2026-05-06.sql.gz
+ */
+const WP_LEGACY_SHORTLINKS: Record<string, string> = {
+  '06735': 'http://poly.rpi.edu/2013/03/06/pss_breaking_the_third_wall/',
+  '38223': 'http://documents.studentsenate.rpi.edu/documents/2482/revisions/current/download',
+  '12311': 'http://docs.studentsenate.rpi.edu/categories/366',
+  '34045': 'http://poly.rpi.edu/2013/11/20/transportation_ideas_discussed/',
+  '88672': 'http://poly.rpi.edu/2014/04/07/grand-marshal-qa/',
+  '21482': 'http://poly.rpi.edu/wp-content/uploads/Decision_3_GM2014.pdf',
+  '52780': 'https://db.tt/xJ0z6OEc',
+  '85211': 'https://db.tt/PuGolFgh',
+  '19218': 'https://db.tt/UCIBxBGY',
+  '35434': 'http://docs.studentsenate.rpi.edu/categories/366?type=grid',
+  '72189': 'https://www.youtube.com/watch?v=ZpnYtQWQES8',
+  '75886': 'http://docs.studentsenate.rpi.edu/documents/2566/revisions/current/download',
+  '36249': 'http://poly.rpi.edu/wp-content/uploads/docs.studentsenate.rpi_.pdf',
+  '08077': 'http://www.nasa.gov/mission_pages/station/main/',
+  '85326': 'http://scte.rpi.edu/foundry.html',
+  '87234': 'https://docs.google.com/document/d/1Zl_TBwR5ICcmS8bh526DJUSeeejs4vDn5Bigwi_dCdo/edit?usp=sharing',
+  '36284': 'http://www.nps.gov/nr/feature/places/13000911.htm',
+  '13210': 'https://www.google.com/calendar/embed?src=rpistudentsenate@gmail.com&ctz=America/New_York',
+  '91063': 'http://poly.rpi.edu/files/GreekLifeCommonsStatement.pdf',
+  '19012': 'http://poly.rpi.edu/files/RecruitmentBylawsApproved05.08.2013.pdf',
+  '29919': 'https://docs.google.com/forms/d/1-rwKTH03e8RhKi_9ewLkDpx0kSdV9UNruyi31oFCDEc/viewform',
+  '09925': 'http://poly.rpi.edu/files/SiSTTTC-TC-r1.pdf',
+  '90835': 'http://www.rpiathletics.com/schedule.aspx?path=hockey',
+  '47288': 'http://docs.studentsenate.rpi.edu/categories/401',
+  '22763': 'http://www.signupgenius.com/go/10c044aaea92caafc1-reunion',
+  '83842': 'http://harvestmoonpainting.eventbrite.com',
+  '83483': 'http://conta.cc/1lWpAhY',
+  '95508': 'http://www.facebook.com/hybridelectronicsandcharacterizationlab',
+  '82923': 'http://rpi.edu/~lewisk2',
+  '68268': 'https://docs.google.com/forms/d/1UXBlf4V8sxRlVOj_XRP5b0OIra1zKizyT_QX3uI7nnc/viewform',
+  '42183': 'http://198.57.203.133/~princfj9/wp/',
+  '26062': 'http://rpiecoheroes.wikispaces.com/home',
+  '17163': 'http://asme.union.rpi.edu/pumpkin.html',
+}
 
 type CacheEntry = { gone: boolean; expiresAt: number }
 const statusCache = new Map<string, CacheEntry>()
@@ -93,6 +142,15 @@ async function lookupLegacyWpRedirect(
 }
 
 export async function middleware(req: NextRequest) {
+  // Legacy WP shortlink (5-digit IDs from pluginSL_shorturl) → original URL.
+  const shortMatch = req.nextUrl.pathname.match(LEGACY_WP_SHORTLINK_RE)
+  if (shortMatch) {
+    const target = WP_LEGACY_SHORTLINKS[shortMatch[1]]
+    if (target) {
+      return NextResponse.redirect(target, 301)
+    }
+  }
+
   // Old WP permalink → new polymer URL (preserves SEO from the 2009-2019 era).
   const wpMatch = req.nextUrl.pathname.match(LEGACY_WP_URL_RE)
   if (wpMatch) {
