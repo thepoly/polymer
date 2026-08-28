58109e1b6cd94bfe76ee302627bca00a1e0a0363
Meher <randem@rpi.edu>
2026-08-28 16:28:39 -0400

    updated location

--- app/(frontend)/about/page.tsx
+++ app/(frontend)/about/page.tsx
@@ -16,15 +16,14 @@ export default async function About() {
                 Printed issues can be viewed at the Institute Archives &amp; Special Collections in the Folsom Library.
             </p>
             <p className="my-5 leading-8">
-                As of August 2019, our office is now located in RU 3510/3511, which is next to the elevator on the third floor of the Rensselaer Union. <br /> 
+                As of April 2026, our office is now located in RU 3324, which is in the hallway to the left of the elevator on the third floor of the Rensselaer Union. <br /> 
             </p>
             <h1 className = {`mt-10 text-4xl font-serif mb-4 capitalize text-accent`}>
                     {"Meeting Times"}
             </h1>
             <p className="leading-8">
-                <i>The Poly</i> meets twice a week in RU 3510/3511 on Sunday at 1 pm, and on Monday at 6 pm. Everyone is welcome to attend. 
-                Meeting times are subject to change, so please contact poly@rpi.edu in advance if you plan on coming or would prefer to attend meetings online.
-            </p>
+                <i>The Poly</i> meets twice a week in RU 3324 on Sunday at 1 pm, and on Monday at 6 pm. Everyone is welcome to attend. 
+                Meeting times are subject to change, and we will notify as such on our Discord server.
         </div>
       </div>
   </main>);
